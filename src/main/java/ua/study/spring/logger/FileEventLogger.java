acb8be5f570f4a6da25b89b0120e5bd548ac239d
tymchenko <tymchenko@gmail.com>
2018-07-16 03:22:03 +0300

    created init-method for FileEventLogger class and added init method to spring.xml

--- src/main/java/ua/study/spring/logger/FileEventLogger.java
+++ src/main/java/ua/study/spring/logger/FileEventLogger.java
@@ -7,17 +7,24 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileEventLogger implements EventLogger {
-    private String fileName;
+    private File file;
 
     public FileEventLogger(String fileName) {
-        this.fileName = fileName;
+        file = new File(fileName);
+
     }
 
     public void logEvent(Event event) {
         try {
-            FileUtils.writeStringToFile(new File(fileName), event.toString());
+            FileUtils.writeStringToFile(file, event.toString());
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void init(){
+        if(!file.canWrite()){
+            file.setWritable(true);
         }
     }
 }
