1ba2d35788a569fd024eff5cb04c09e286da4c13
tymchenko <tymchenko@gmail.com>
2018-07-16 03:37:07 +0300

    changed applicationContext in Main class to ConfigurableApplivcationContext and added ctx.close() method in main method

--- src/main/java/ua/study/spring/Main.java
+++ src/main/java/ua/study/spring/Main.java
@@ -1,14 +1,16 @@
 package ua.study.spring;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
 
         Application application = (Application)ctx.getBean("application");
         application.logEvent();
+
+        ctx.close();
     }
 }
