28fb096749cb3f9f537ce1d586fd134d9c7f55f0
tymchenko <tymchenko@gmail.com>
2018-07-16 03:33:21 +0300

    created CacheFileLogger class which extends FileEventLogger and created cash, cashSize fields with logEvent(Event) and writeEventFromFile() methods

---
+++ src/main/java/ua/study/spring/logger/CacheFileLogger.java
@@ -0,0 +1,29 @@
+package ua.study.spring.logger;
+
+import ua.study.spring.beans.Event;
+
+import java.util.List;
+
+public class CacheFileLogger extends FileEventLogger {
+    private int casheSize;
+    private List<Event> cache;
+
+    public CacheFileLogger(String fileName, int casheSize) {
+        super(fileName);
+        this.casheSize = casheSize;
+    }
+
+    public void logEvent(Event event){
+        cache.add(event);
+        if(cache.size() == casheSize){
+            writeEventFromCashe();
+            cache.clear();
+        }
+    }
+
+    private void writeEventFromCashe() {
+        for(Event event : cache){
+            super.logEvent(event);
+        }
+    }
+}
