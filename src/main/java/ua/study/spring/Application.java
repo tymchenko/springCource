68468e236dcf2ed62b7ef97c51735e3fffd04283
tymchenko <tymchenko@gmail.com>
2018-08-10 20:53:32 +0300

    updated application class

--- src/main/java/ua/study/spring/Application.java
+++ src/main/java/ua/study/spring/Application.java
@@ -2,27 +2,28 @@ package ua.study.spring;
 
 import ua.study.spring.beans.Client;
 import ua.study.spring.event.EventType;
-import ua.study.spring.logger.ConsoleEventLogger;
 import ua.study.spring.logger.EventLogger;
 
 import java.util.Map;
 
 public class Application {
     Client client;
-    EventLogger log;
+    EventLogger defaultLogger;
     Map<EventType, EventLogger> loggers;
 
-    public Application(Client client, EventLogger log, Map<EventType, EventLogger> loggers){
+
+    public Application(Client client, EventLogger defaultLogger, Map<EventType, EventLogger> loggers){
         this.client = client;
-        this.log = log;
+        this.defaultLogger = defaultLogger;
         this.loggers = loggers;
     }
 
     public void logEvent(EventType type, String msg){
         EventLogger logger = loggers.get(type);
         if(logger == null){
-            logger = new ConsoleEventLogger();
+            logger = defaultLogger;
         }
-        logger.logEvent(client.getName());
+        //todo logger.logEvent(event);
+        logger.logEvent(msg);
     }
 }
